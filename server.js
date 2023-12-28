d72a2fa02986fc810b7feda41a36a28621cc0ccc
Conor <pacman1444@gmail.com>
2023-12-28 22:21:00 +0000

    Imported mongodb, mysql & ejs. Added home.ejs with links to the three pages

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+//Import modules
 const express = require('express')
 const app = express()
 const port = 3000
+const ejs = require('ejs');
+
+//Use EJS to display html
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
+
+//Basic feedback for server console
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
