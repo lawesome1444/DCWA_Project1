7b47ba5bc0ccd65c7c87e14d81cd287268caf8b0
Conor <pacman1444@gmail.com>
2023-12-29 17:45:21 +0000

    server now handles localhost/stores requests and is setup for SQL query

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 const ejs = require('ejs');
+const sqlDAO = require('./mySQLDAO')
 
 //Use EJS to display html
 app.set('view engine', 'ejs');
 
-
+//Display home.ejs if at localhost:3000
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+//Display the store.ejs page
+app.get('/stores', async (req, res) =>{
+  //Attempt to call the listStores function in the SQL DAO
+  try{
+    var allStores = await sqlDAO.listStores();
+    res.render('stores', {allStores : allStores});
+  }
+  //if unsuccesful
+  catch (err){
+    console.error(err);
+    res.status(500).send('SQL Connection Pool error');
+  }
 })
 
 
